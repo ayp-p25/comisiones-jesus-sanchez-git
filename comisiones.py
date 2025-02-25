78da0a2ab1549c351a85a60cdb9b02d45ddef6e7
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-02-25 00:14:56 -0600

    First

--- comisiones.py
+++ comisiones.py
@@ -1,15 +1,13 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+L = float(input("Ingrese las ventas del lunes : \n"))
+M = float(input("Ingrese las ventas del Martes : \n"))
+Mi = float(input("Ingrese las ventas del miercoles : \n"))
+J = float(input("Ingrese las ventas del jueves : \n"))
+V = float(input("Ingrese las ventas del viernes : \n"))
+VT = L+M+Mi+J+V
+if VT < 20000 :
+    Comision = 0.05
+else :
+    Comision = 0.06
+V_Comision = VT*Comision
+print("La venta total es de :" , VT)
+print("La comision total es de : ", V_Comision)
